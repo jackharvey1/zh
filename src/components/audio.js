e2997ce6ac8ecc47e2e452fda9d1b8c401dde9a8
Jack Harvey <jackrrv@gmail.com>
2024-09-08 16:35:28 +0100

    Add audio with guessing functionality

--- src/components/audio.js
+++ src/components/audio.js
@@ -1,13 +1,21 @@
 import './stylesheet.css';
 
-export default () => (
-    <div className="audio-container">
-        <p className="audio-text">Tap the character you hear</p>
-        <span className="audio-image">
-            <img
-                alt="audio icon"
-                src={process.env.PUBLIC_URL + '/speaker.svg'}
-            />
-        </span>
-    </div>
-);
+export default ({ pathToAudio }) => {
+    const audio = new Audio(pathToAudio);
+
+    function play() {
+        audio.play();
+    }
+
+    return (
+        <div className="audio-container">
+            <p className="audio-text">Tap the character you hear</p>
+            <span className="audio-image" onClick={play}>
+                <img
+                    alt="audio icon"
+                    src={process.env.PUBLIC_URL + '/speaker.svg'}
+                />
+            </span>
+        </div>
+    );
+};
